8c1be37a69f09ab8428830acf58412d817ab5ec7
aleberas <alessia.berarducci@studio.unibo.it>
2025-05-09 18:31:25 +0200

    Create ols.R
    ols rscript

---
+++ functions/ols.R
@@ -0,0 +1,15 @@
+##########################################
+# OLS ####################################
+##########################################
+
+library(CVXR)
+
+compute_ols <- function(m) {
+  
+  beta <- Variable(rows = m$p)
+  objective <- Minimize(sum_squares(m$Gplus %*% beta - m$Zplus))
+  problem <- Problem(objective)
+  sol <- solve(problem)
+  
+  return(sol$getValue(beta))
+}
