8f22800ef9c613431226ff80c0bd84d28d5bfbd4
aleberas <alessia.berarducci@studio.unibo.it>
2025-06-23 17:04:08 +0200

    Update population_study.R

--- scripts/population_study.R
+++ scripts/population_study.R
@@ -2,6 +2,9 @@
 ###### POPULATION STUDY ####################
 ###### NOT ALIGNED SHIFT ##################
 ############################################
+############################################
+###### POPULATION STUDY ####################
+############################################
 
 source("./libraries.R")
 
@@ -49,9 +52,7 @@ for (i in 1:length(beta1)) {
     R_delta_g[i,j]<-(beta_vec- beta_cp)%*%G_delta_plus%*%t(beta_vec- beta_cp)
   }
 }
-
-
-
+ub<-(Py-beta_vec%*%Px)%*%solve(I-B)
 
 # Abs(Rdelta) & Rdelta+
 
@@ -113,7 +114,7 @@ p <- p %>% add_markers(
 )
 p <- p %>% layout(
   title = list(
-    text = "Surface Plot of RΔ and |RΔ|",
+   # text = "Surface Plot of RΔ and |RΔ|",
     x = 0.5,                  
     y = 0.92,                 
     xanchor = "center",       
@@ -134,6 +135,13 @@ p
 
 
 # Rdelta+ and R_delta_G
+beta_ols<-solve(Gplus,Zplus)
+ub<-(Py-t(beta_ols)%*%Px)%*%solve(I-B)
+R_delta_plus_ols<-ub%*%F_Delta_plus%*%t(ub) 
+R_delta_plus_ols
+R_delta_g_ols<-(t(beta_ols)- beta_cp)%*%G_delta_plus%*%t(t(beta_ols)- beta_cp)
+R_delta_g_ols
+
 
 p <- plot_ly()
 p <- p %>% add_surface(   x = ~beta1,   y = ~beta2,   z = ~R_delta_g,  colorscale = list(c(0, "lightgreen"), c(1, "darkgreen")),   showscale = TRUE,   colorbar = list(title = "RΔG", tickfont = list(color =  "darkgreen"),                   titlefont = list(color =  'darkgreen')),   contours = list(     x = list(show = TRUE, color = "black"),     y = list(show = TRUE, color = "black"),     z = list(show = TRUE, color = "black")   ) ) 
@@ -145,6 +153,20 @@ p <- p %>% add_markers(
   z = 0,
   marker = list(size = 8, color = "blue"),
   name = paste("βCP ")
+)
+p <- p %>% add_markers(
+  x = beta_ols[1,],
+  y = beta_ols[2,],
+  z = R_delta_plus_ols,
+  marker = list(size = 8, color = "red"),
+  name = paste("βOLS2 ")
+)
+p <- p %>% add_markers(
+  x = beta_ols[1,],
+  y = beta_ols[2,],
+  z = R_delta_g_ols,
+  marker = list(size = 8, color = "yellow"),
+  name = paste("βOLS1 ")
 )
 p <- p %>% layout(
   title = list(
@@ -173,6 +195,10 @@ p
 
 
 
+
+###########
+
+
 # Theoretical Contour 
 
 contour(beta1, beta2, Rdelta, level=0,xlab="β1",ylab="β2", main="Contour Plot of Population RΔ")
@@ -181,8 +207,8 @@ contour(beta1, beta2, Rdelta, level=0,xlab="β1",ylab="β2", main="Contour Plot 
 
 
 
-#gamma<-seq(0,50,length=3000)
-gamma<-seq(0,10,length.out=5000)
+#gamma<-seq(0,500,length=30000)
+gamma<-seq(0,100,length.out=5000)
 gamma<-c(gamma,100,1000,10000)
 
 beta_min<-matrix(NA,nrow=2,ncol=length(gamma))
@@ -245,18 +271,18 @@ plot(beta_min[1,],beta_min[2,],type='p',xlim=c(0.4,1.5),ylim=c(-1,1.2),xlab='β�
 contour(beta1, beta2, Rdelta, level=0,xlab="β1",ylab="β2", add=TRUE)
 
 
+
+
 #########################################
 ###### NEW beta_gamma ALGORITHM #########
-###### POPULATION CASE ##################
+###### EMPIRICAL CASE ##################
 #########################################
 
 beta_gamma_G<-matrix(NA,nrow=2,ncol=length(gamma))
 
-
-
-#gamma<-seq(0,50,length=3000)
-gamma<-seq(0,10,length.out=5000)
+gamma<-seq(0,100,length.out=5000)
 gamma<-c(gamma,100,1000,10000)
+#gamma<-seq(0,500,length=30000)
 
 R_delta_G <- numeric(length(gamma))  
 
@@ -282,25 +308,139 @@ contour(beta1, beta2, Rdelta, level = 0, xlab = "β1", ylab = "β2", add = TRUE)
 
 
 
+
+
+###### THEORETICAL BETAGAMMA FROM RDELTA+ 
+gamma<-seq(0,100,length.out=5000)
+gamma<-c(gamma,100,1000,10000)
+
+Zdelta <- matrix(c(1, -1), nrow = 2, byrow = TRUE)
+Gdelta <- matrix(c(1, -1, -1, 0), nrow = 2, byrow = TRUE)
+Zplus <- matrix(c(3, -5), ncol = 1)
+Gplus <- matrix(c(3, -3, -3, 8), nrow = 2, byrow = TRUE)
+
+
+eigen_Gdelta <- eigen(Gdelta)
+V <- eigen_Gdelta$vectors
+Lambda <- diag(abs(eigen_Gdelta$values)) 
+G_delta_plus <- V %*% Lambda %*% t(V)
+
+
+F_Delta_plus <- matrix(c(1, 0, 0,
+                         0, 1, 0,
+                         0, 0, 0), byrow = TRUE, nrow = 3)
+
+
+beta1 <- seq(0, 2, length.out = 100)
+beta2 <- seq(-1, 1, length.out = 100)
+beta <- expand.grid(beta1 = beta1, beta2 = beta2)
+
+
+Rdelta <- matrix(0, nrow = length(beta1), ncol = length(beta2))
+R_delta_plus <- matrix(0, nrow = length(beta1), ncol = length(beta2))
+R_delta_g <- matrix(0, nrow = length(beta1), ncol = length(beta2))
+
+
+p <- 2
+B <- matrix(c(0, 0, 0,
+              0, 0, -1,
+              1, 0, 0),
+            nrow = p + 1, byrow = TRUE)
+I <- diag(p + 1)
+Px <- matrix(c(1, 0, 0,
+               0, 1, 0),
+             nrow = p, ncol = p + 1, byrow = TRUE)
+Py <- matrix(c(0, 0, 1), nrow = 1, ncol = p + 1)
+beta_cp <- matrix(c(1, 0), nrow = 1)
+
+# Loop over grid and compute values
+for (i in 1:length(beta1)) {
+  for (j in 1:length(beta2)) {
+    beta_vec <- c(beta1[i], beta2[j])
+    term1 <- 2 * sum(Zdelta * beta_vec)
+    term2 <- t(beta_vec) %*% Gdelta %*% beta_vec
+    Rdelta[i, j] <- 1 - term1 + term2
+    
+    ub <- (Py - beta_vec %*% Px) %*% solve(I - B)
+    R_delta_plus[i, j] <- ub %*% F_Delta_plus %*% t(ub)
+    R_delta_g[i, j] <- (beta_vec - beta_cp) %*% G_delta_plus %*% t(beta_vec - beta_cp)
+  }
+}
+
+
+
+
+# Compute beta_gamma
+u_x <- Px %*% solve(I - B)
+u_y <- Py %*% solve(I - B)
+# Loop over all gamma values
+# Preallocate a matrix to store beta_gamma for each gamma
+beta_gamma_all <- matrix(0, nrow = 2, ncol = length(gamma))
+
+for (g in 1:length(gamma)) {
+  A <-  gamma[g] * u_x %*% F_Delta_plus %*% t(u_x)
+  b <- Zplus +  gamma[g] * u_x %*% F_Delta_plus %*% t(u_y)
+  beta_gamma_new <- solve(Gplus + A, b)
+  beta_gamma_all[,g] <- beta_gamma_new[, 1]  
+}
+
+plot(beta_gamma_all[1,],beta_gamma_all[2,],type='l',xlab='β1',ylab='β2')
+contour(beta1,beta2,Rdelta,level=0,add=TRUE)
+
+
+
+R_delta_plus_gamma <- numeric(length(gamma))
+
+IB_inv <- solve(I - B)
+
+for (g in 1:length(gamma)) {
+  beta_gamma <- beta_gamma_all[, g, drop = FALSE]  # 2x1 column vector
+  u_beta <- (Py - t(beta_gamma) %*% Px) %*% IB_inv
+  R_delta_plus_gamma[g] <- u_beta %*% F_Delta_plus %*% t(u_beta)
+}
+
+plot(gamma,R_delta_plus_gamma,type='l',xlab='γ',ylab='RΔ+',main='RΔ+ vs γ',cex=0.7,xlim=c(1,100))
+
+
+
+### PLOTSS
+
 # TOGETHER beta_gammaG and beta_gamma_min
 
 plot(beta_gamma_G[1, ], beta_gamma_G[2, ],
-     type = 'p', xlim = c(0.4, 1.2), ylim = c(-0.6, 1.1),
+     type = 'l', xlim = c(0.4, 1.2), ylim = c(-0.6, 1.1),
      xlab = 'βγ1', ylab = 'βγ2', cex = 0.7,
-     main = 'βγG vs βγ-')
+    # main = 'βγG, βγ- & βγ, Population Study',
+    lwd=3)
 contour(beta1, beta2, Rdelta, level = 0, add = TRUE)
 
-points(beta_min[1, ], beta_min[2, ],
-       type = 'p', cex = 0.5, col = 'red', pch = 8)
+lines(beta_min[1, ], beta_min[2, ],
+      type = 'l', cex = 0.5, col = 'darkorange', pch = 8,lwd=3)
+lines(beta_gamma_all[1,],beta_gamma_all[2,],type='l',xlab='β1',ylab='β2',col='blue',lty=22,lwd=3)
+
+
 legend("topleft",
-       legend = c("βγG", "βγ-"),
-       col = c("black", "red"),
+       legend = c("βγG", "βγ-", "βγ"),
+       col = c("black", "darkorange","blue"),
        cex=0.9,
-       pch = c(1, 3),
-       y.intersp = 0.3,  
-       pt.cex = 0.7,
-       x.intersp = 0.3,
+       lty=c(1,1,22),
+       lwd=c(3,3,3),
+       y.intersp = 0.6,  
+       x.intersp = 0.5,
        bty = "o")
+# |Rdelta| e Rdelta^+ uguali perchè matrice diag 
 
+plot(gamma,R_delta_G,type='l',ylab='RΔG,|RΔ| & RΔ+',xlab='γ',lwd=3,xlim=c(0,30),cex=0.4,ylim=c(-0.05,0.25))
 
+lines(gamma,abs(R_delta_min), type = 'l', cex = 0.5, col = 'darkorange', lwd= 3)
+lines(gamma,R_delta_plus_gamma, type = 'l', cex = 0.5, col = 'blue', lty= 22,lwd=3)
+legend("topright",
+       legend = c("RΔG","|RΔ|", "RΔ+"),
+       col = c("black", "darkorange", "blue"),
+       lty = c(1, 1, 22),
+       lwd = c(3, 3, 3),
+       cex = 0.9,
+       y.intersp = 0.6,
+       x.intersp = 0.5,
+       bty = "o")
 
